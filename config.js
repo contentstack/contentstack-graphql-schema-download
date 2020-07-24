4e4c138d48bade894e736ac02b82fd019d976310
Uttam K Ukkoji <uttamukkoji@gmail.com>
2020-07-24 11:53:34 +0530

    Config updated

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+   host: '<HOST>',
+   api_key: '<STACK_API_KEY>',
+   deliveryToken: '<ENVIRONMENT_SPECIFIC_DELIVERY_TOKEN>',
+   environment: '<ENVIRONMENT>',
+   contentTypes: '<NUMBER_OF_CONTENT_TYPES_IN_STACK>',
+   fileName: '<SCHEMA_FILE_NAME>'
+}
+module.exports = { config }
